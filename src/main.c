8780674d71d3d2a8723dacd8b5e2cf7e9bb230c3
Matheus Mendes <mendess.mathews@usp.br>
2026-04-15 09:13:19 -0300

    led laranja ligado

--- src/main.c
+++ src/main.c
@@ -1,30 +1,26 @@
-#include <zephyr/kernel.h>             
-#include <zephyr/device.h>             
-#include <zephyr/drivers/gpio.h>      
-#include <pwm_z42.h>                
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <pwm_z42.h>
 
-
-#define TPM_MODULE 1000             
+#define TPM_MODULE 1000
 
 int main(void)
 {
-    
     pwm_tpm_Init(TPM2, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
 
-pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 19);
+    pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 18);
+    pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 19);
 
-pwm_tpm_CnV(TPM2, 1, 0);
+    pwm_tpm_CnV(TPM2, 0, 0);
+    pwm_tpm_CnV(TPM2, 1, 600);
 
     for (;;)
     {
-        k_msleep(2000);
+        
+        pwm_tpm_CnV(TPM2, 0, 0);
+        pwm_tpm_CnV(TPM2, 1, 600);
 
-        pwm_tpm_CnV(TPM2, 1, 0);
-
-         k_msleep(2000);
-
-        pwm_tpm_CnV(TPM2, 1, 1000);
-    
     }
 
     return 0;
