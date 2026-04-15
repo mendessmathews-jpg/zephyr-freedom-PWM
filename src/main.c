f2a6757144c5a4d882a8aadf61d6163cdffbb641
Matheus Mendes <mendess.mathews@usp.br>
2026-04-15 08:34:02 -0300

    led verde piscando 1000ms

--- src/main.c
+++ src/main.c
@@ -1,32 +1,30 @@
-#include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/gpio.h>
-#include <pwm_z42.h>
+#include <zephyr/kernel.h>             
+#include <zephyr/device.h>             
+#include <zephyr/drivers/gpio.h>      
+#include <pwm_z42.h>                
 
-#define TPM_MODULE 1000
+
+#define TPM_MODULE 1000             
 
 int main(void)
 {
+    
     pwm_tpm_Init(TPM2, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
 
-    pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 18);
-    pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 19);
+pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 19);
 
-    pwm_tpm_CnV(TPM2, 0, 0);
-    pwm_tpm_CnV(TPM2, 1, 600);
+pwm_tpm_CnV(TPM2, 1, 0);
 
     for (;;)
     {
         k_msleep(1000);
-        
-        pwm_tpm_CnV(TPM2, 0, 0);
-        pwm_tpm_CnV(TPM2, 1, 600);
 
-        k_msleep(1000);
+        pwm_tpm_CnV(TPM2, 1, 0);
 
-        pwm_tpm_CnV(TPM2, 0, 1000);
+         k_msleep(1000);
+
         pwm_tpm_CnV(TPM2, 1, 1000);
-
+    
     }
 
     return 0;
